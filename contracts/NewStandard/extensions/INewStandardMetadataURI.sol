80532fc99eca85b515e2bc0798bd21a69a5e4a2a
Mohamad Hammoud <mohamad.hammoudk@gmail.com>
2023-03-27 10:43:02 +0300

    ⚰️ Delete New Standard

--- contracts/NewStandard/extensions/INewStandardMetadataURI.sol
+++
@@ -1,9 +0,0 @@
-// SPDX-License-Identifier: MIT
-
-pragma solidity ^0.8.14;
-
-import "../INewStandard.sol";
-
-interface INewStandardMetadataURI is INewStandard {
-    function uri(uint256 mainId) external view returns (string memory);
-}
