b9c8359d878a370823106a7222ab408bea87daa2
Mohamad Hammoud <mohamad.hammoudk@gmail.com>
2023-03-28 15:00:15 +0300

    ✨ introduce invoice contract

--- test/DLT.js
+++ test/DLT.js
@@ -1,7 +1,7 @@
 const { expect } = require("chai");
 const { ethers } = require("hardhat");
 
-describe("Start Point", async function () {
+describe("DLT", async function () {
   let DLT;
 
   let owner;
