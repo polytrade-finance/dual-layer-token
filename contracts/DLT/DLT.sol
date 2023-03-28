ac1e16f3f64d040a8a12afb95728c3ebb98b015a
Mohamad Hammoud <mohamad.hammoudk@gmail.com>
2023-03-28 18:38:35 +0300

    🚧 adding mainId, subId to the mint method and resolved data paramereter

--- contracts/DLT/DLT.sol
+++ contracts/DLT/DLT.sol
@@ -53,8 +53,7 @@ contract DLT is IDLT {
      *
      * - `sender` and `recipient` cannot be the zero address.
      * - `sender` must have a balance of at least `amount`.
-     * - the caller must have allowance for ``sender``'s tokens of at least
-     * `amount`.
+     * - the caller must have allowance for `sender`'s tokens of at least `amount`.
      */
     function transferFrom(
         address sender,
@@ -208,7 +207,7 @@ contract DLT is IDLT {
         require(sender != address(0), "DLT: transfer from the zero address");
         require(recipient != address(0), "DLT: transfer to the zero address");
 
-        _beforeTokenTransfer(sender, recipient, mainId, subId, amount);
+        _beforeTokenTransfer(sender, recipient, mainId, subId, amount, "");
 
         uint256 senderBalanceMain = _mainBalances[mainId][sender];
         uint256 senderBalanceSub = _subBalances[mainId][subId][sender];
@@ -224,9 +223,9 @@ contract DLT is IDLT {
         _mainBalances[mainId][recipient] += amount;
         _subBalances[mainId][subId][recipient] += amount;
 
-        emit Transfer(sender, recipient, mainId, subId, amount, "");
+        emit Transfer(sender, recipient, mainId, subId, amount);
 
-        _afterTokenTransfer(sender, recipient, mainId, subId, amount);
+        _afterTokenTransfer(sender, recipient, mainId, subId, amount, "");
     }
 
     /** @dev Creates `amount` tokens and assigns them to `account`
@@ -237,27 +236,30 @@ contract DLT is IDLT {
      *
      * - `account` cannot be the zero address.
      */
-    function _mint(address account, uint256 amount) internal virtual {
+    function _mint(
+        address account,
+        uint256 mainId,
+        uint256 subId,
+        uint256 amount
+    ) internal virtual {
         require(account != address(0), "DLT: mint to the zero address");
 
-        uint256 newMainId = ++_totalMainIds;
-
-        _beforeTokenTransfer(address(0), account, newMainId, 1, amount);
+        _beforeTokenTransfer(address(0), account, mainId, subId, amount, "");
 
         unchecked {
             _totalSupply += amount;
-            _mainTotalSupply[newMainId] += amount;
-            _subTotalSupply[newMainId][1] += amount;
+            _mainTotalSupply[mainId] += amount;
+            _subTotalSupply[mainId][subId] += amount;
 
-            ++_totalSubIds[newMainId];
+            ++_totalSubIds[mainId];
 
-            _mainBalances[newMainId][account] += amount;
-            _subBalances[newMainId][1][account] += amount;
+            _mainBalances[mainId][account] += amount;
+            _subBalances[mainId][subId][account] += amount;
         }
 
-        emit Transfer(address(0), account, newMainId, 1, amount, "");
+        emit Transfer(address(0), account, mainId, subId, amount);
 
-        _afterTokenTransfer(address(0), account, newMainId, 1, amount);
+        _afterTokenTransfer(address(0), account, mainId, subId, amount, "");
     }
 
     /**
@@ -282,7 +284,7 @@ contract DLT is IDLT {
         uint256 fromBalanceSub = _subBalances[mainId][subId][account];
         require(fromBalanceSub >= amount, "DLT: insufficient balance");
 
-        _beforeTokenTransfer(account, address(0), mainId, subId, amount);
+        _beforeTokenTransfer(account, address(0), mainId, subId, amount, "");
 
         unchecked {
             _totalSupply -= amount;
@@ -294,9 +296,18 @@ contract DLT is IDLT {
             // Overflow not possible: amount <= fromBalanceMain <= totalSupply.
         }
 
-        emit Transfer(account, address(0), mainId, subId, amount, "");
+        emit Transfer(account, address(0), mainId, subId, amount);
 
-        _afterTokenTransfer(account, address(0), mainId, subId, amount);
+        _afterTokenTransfer(account, address(0), mainId, subId, amount, "");
+    }
+
+    function _allowance(
+        address owner,
+        address spender,
+        uint256 mainId,
+        uint256 subId
+    ) internal view returns (uint256) {
+        return _allowances[owner][spender][mainId][subId];
     }
 
     /**
@@ -318,13 +329,15 @@ contract DLT is IDLT {
         address recipient,
         uint256 mainId,
         uint256 subId,
-        uint256 amount
+        uint256 amount,
+        bytes memory data
     ) internal virtual {
         sender;
         recipient;
         mainId;
         subId;
         amount;
+        data;
     }
 
     /**
@@ -346,21 +359,14 @@ contract DLT is IDLT {
         address recipient,
         uint256 mainId,
         uint256 subId,
-        uint256 amount
+        uint256 amount,
+        bytes memory data
     ) internal virtual {
         sender;
         recipient;
         mainId;
         subId;
         amount;
-    }
-
-    function _allowance(
-        address owner,
-        address spender,
-        uint256 mainId,
-        uint256 subId
-    ) internal view returns (uint256) {
-        return _allowances[owner][spender][mainId][subId];
+        data;
     }
 }
