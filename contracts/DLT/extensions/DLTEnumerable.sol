b96329fbf21225e090b62dc73240f054cb172b2f
saxon <1@1.com>
2023-05-05 16:18:18 +0330

    🔨 overriding mint and burn function

--- contracts/DLT/extensions/DLTEnumerable.sol
+++ contracts/DLT/extensions/DLTEnumerable.sol
@@ -11,20 +11,30 @@ import "../interfaces/IDLTEnumerable.sol";
 abstract contract DLTEnumerable is DLT, IDLTEnumerable {
     uint256 private _totalMainIds;
 
-    /// MappiArray of sub ids for each main ids
+    /**
+     * @dev MappiArray of sub ids for each main ids
+     */
     mapping(uint256 => uint256) private _totalSubIds;
 
-    /// Mapping from mainId to total supply of main ids
+    /**
+     * @dev Mapping from mainId to total supply of main ids
+     */
     mapping(uint256 => uint256) private _mainTotalSupply;
 
-    /// Mapping from mainId to sub id to total supply of sub ids
+    /**
+     * @dev Mapping from mainId to sub id to total supply of sub ids
+     */
     mapping(uint256 => mapping(uint256 => uint256)) private _subTotalSupply;
 
-    /// Mapping from main ids to address to array of sub ids
-    mapping(uint256 => mapping(address => uint256[])) private _ownedSubIds;
+    /**
+     * @dev Mapping from main ids to array of sub ids
+     */
+    mapping(uint256 => uint256[]) private _subIds;
 
-    /// Mapping from subId to position in the _subIds array
-    mapping(uint256 => uint256) private _subIdIndex;
+    /**
+     * @dev Mapping from subId to position in the _subIds array
+     */
+    mapping(uint256 => mapping(uint256 => uint256)) private _subIdIndex;
 
     /**
      * @dev See {IDLTEnumerable-totalMainIds}.
@@ -36,9 +46,7 @@ abstract contract DLTEnumerable is DLT, IDLTEnumerable {
     /**
      * @dev See {IDLTEnumerable-totalSubIds}.
      */
-    function totalSubIds(
-        uint256 mainId
-    ) public view virtual override returns (uint256) {
+    function totalSubIds(uint256 mainId) public view virtual returns (uint256) {
         return _totalSubIds[mainId];
     }
 
@@ -47,7 +55,7 @@ abstract contract DLTEnumerable is DLT, IDLTEnumerable {
      */
     function mainTotalSupply(
         uint256 mainId
-    ) public view virtual override returns (uint256) {
+    ) public view virtual returns (uint256) {
         return _mainTotalSupply[mainId];
     }
 
@@ -57,129 +65,88 @@ abstract contract DLTEnumerable is DLT, IDLTEnumerable {
     function subTotalSupply(
         uint256 mainId,
         uint256 subId
-    ) public view virtual override returns (uint256) {
+    ) public view virtual returns (uint256) {
         return _subTotalSupply[mainId][subId];
     }
 
     /**
-     * @dev See {IDLTEnumerable-ownedSubIds}.
+     * @dev See {IDLTEnumerable-subIds}.
      */
-    function ownedSubIds(
-        uint256 mainId,
-        address owner
-    ) public view virtual override returns (uint256[] memory) {
-        return _ownedSubIds[mainId][owner];
+    function subIds(
+        uint256 mainId
+    ) public view virtual returns (uint256[] memory) {
+        return _subIds[mainId];
     }
 
     /**
-     * @dev See {IDLTEnumerable-subIdBalance}.
+     * @dev See {IDLTEnumerable-totalSubIdBalance}.
      */
-    function ownedSubIdBalance(
-        uint256 mainId,
-        address owner
-    ) public view virtual override returns (uint256 totalBalance) {
-        for (uint256 i = 0; i < _ownedSubIds[mainId][owner].length; i++) {
-            totalBalance += _ownedSubIds[mainId][owner][i];
+    function totalSubIdBalance(
+        address owner,
+        uint256 mainId
+    ) public view virtual returns (uint256 totalBalance) {
+        for (uint256 i = 0; i < _subIds[mainId].length; ) {
+            uint256 subId = _subIds[mainId][i];
+            totalBalance += _balances[mainId][owner][subId];
+            unchecked {
+                ++i;
+            }
         }
         return totalBalance;
     }
 
     /**
-     * @dev See {DLT-_beforeTokenTransfer}.
+     * @dev See {DLT-_mint}.
+     * @dev Private function that updates total supply and amount of each ids after minting
      */
-    function _beforeTokenTransfer(
-        address sender,
+    function _mint(
         address recipient,
         uint256 mainId,
         uint256 subId,
-        uint256 amount,
-        bytes memory data
-    ) internal virtual override {
-        super._beforeTokenTransfer(
-            sender,
-            recipient,
-            mainId,
-            subId,
-            amount,
-            data
-        );
-        if (sender == address(0)) {
-            if (_mainTotalSupply[mainId] == 0) {
-                ++_totalMainIds;
-            }
-            if (_subTotalSupply[mainId][subId] == 0) {
-                ++_totalSubIds[mainId];
-            }
-            _addToSubIds(mainId, subId, recipient);
-            _mainTotalSupply[mainId] += amount;
-            _subTotalSupply[mainId][subId] += amount;
-        } else if (sender != recipient) {
-            _addToSubIds(mainId, subId, recipient);
+        uint256 amount
+    ) internal virtual override(DLT) {
+        if (_mainTotalSupply[mainId] == 0) {
+            ++_totalMainIds;
         }
+        if (_subTotalSupply[mainId][subId] == 0) {
+            ++_totalSubIds[mainId];
+            uint256[] storage array = _subIds[mainId];
+            _subIdIndex[mainId][subId] = array.length;
+            array.push(subId);
+        }
+        _mainTotalSupply[mainId] += amount;
+        _subTotalSupply[mainId][subId] += amount;
+        super._mint(recipient, mainId, subId, amount);
     }
 
     /**
-     * @dev See {DLT-_afterTokenTransfer}.
+     * @dev See {DLT-_burn}.
+     * @dev Private function that updates total supply and amount of each ids after burning
      */
-    function _afterTokenTransfer(
-        address sender,
+    function _burn(
         address recipient,
         uint256 mainId,
         uint256 subId,
-        uint256 amount,
-        bytes memory data
-    ) internal virtual override {
-        super._afterTokenTransfer(
-            sender,
-            recipient,
-            mainId,
-            subId,
-            amount,
-            data
-        );
-        if (recipient == address(0)) {
-            unchecked {
-                _mainTotalSupply[mainId] -= amount;
-                _subTotalSupply[mainId][subId] -= amount;
-                // Overflow not possible: amount <= fromBalanceMain <= totalSupply.
-            }
-            _removeFromSubIds(mainId, subId, sender);
-            if (_subTotalSupply[mainId][subId] == 0) {
-                --_totalSubIds[mainId];
-            }
-            if (_mainTotalSupply[mainId] == 0) {
-                --_totalMainIds;
-            }
-        } else if (recipient != sender) {
-            _removeFromSubIds(mainId, subId, sender);
+        uint256 amount
+    ) internal virtual override(DLT) {
+        super._burn(recipient, mainId, subId, amount);
+        unchecked {
+            _mainTotalSupply[mainId] -= amount;
+            _subTotalSupply[mainId][subId] -= amount;
+            // Overflow not possible: amount <= fromBalanceMain <= totalSupply.
         }
-    }
-
-    function _addToSubIds(
-        uint256 mainId,
-        uint256 subId,
-        address recipient
-    ) private {
-        if (_balances[mainId][recipient][subId] == 0) {
-            uint256[] storage array = _ownedSubIds[mainId][recipient];
-            _subIdIndex[subId] = array.length;
-            array.push(subId);
-        }
-    }
-
-    function _removeFromSubIds(
-        uint256 mainId,
-        uint256 subId,
-        address sender
-    ) private {
-        if (_balances[mainId][sender][subId] == 0) {
-            uint256[] storage array = _ownedSubIds[mainId][sender];
-            uint256 subIdIndex = _subIdIndex[subId];
+        if (_subTotalSupply[mainId][subId] == 0) {
+            --_totalSubIds[mainId];
+            uint256[] storage array = _subIds[mainId];
+            uint256 subIdIndex = _subIdIndex[mainId][subId];
             uint256 lastSubId = array[array.length - 1];
             array[subIdIndex] = lastSubId;
-            _subIdIndex[lastSubId] = subIdIndex;
-            delete _subIdIndex[subId];
+            _subIdIndex[mainId][lastSubId] = subIdIndex;
+            delete _subIdIndex[mainId][subId];
             array.pop();
+        }
+        if (_mainTotalSupply[mainId] == 0) {
+            --_totalMainIds;
         }
     }
 }
