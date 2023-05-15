f22e17e277dc7d4d17d48e4b1aed9f45923f5e4d
Mohamad Hammoud <mohamad.hammoudk@gmail.com>
2023-05-15 16:23:34 +0300

    💡 altered "to" to "recipient"

--- contracts/DLT/DLT.sol
+++ contracts/DLT/DLT.sol
@@ -298,7 +298,7 @@ contract DLT is Context, IDLT {
      *
      * Requirements:
      *
-     * - If `to` refers to a smart contract, it must implement {IDLTReceiver-onDLTReceived} and return the
+     * - If `recipient` refers to a smart contract, it must implement {IDLTReceiver-onDLTReceived} and return the
      * acceptance magic value.
      */
     function _safeBatchTransferFrom(
@@ -509,7 +509,7 @@ contract DLT is Context, IDLT {
      * @dev Destroys `amount` tokens from `account`, reducing the
      * total supply.
      *
-     * Emits a {Transfer} event with `to` set to the zero address.
+     * Emits a {Transfer} event with `recipient` set to the zero address.
      *
      * Requirements:
      *
